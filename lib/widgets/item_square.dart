62f3a91358fc1ae0bd7adb775a82e2a64152cb22
kellyycha <65841983+kellyycha@users.noreply.github.com>
2024-03-29 12:35:54 -0400

    item card spacing stays consistent when deleted

--- lib/widgets/item_square.dart
+++ lib/widgets/item_square.dart
@@ -21,127 +21,125 @@ class MySquare extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Dismissible(
-        key: UniqueKey(),
-        onDismissed: (_) => onDelete(),
-        direction: DismissDirection.endToStart,
-        background: Container(
-          alignment: Alignment.centerRight,
-          padding: const EdgeInsets.only(right: 20.0),
-          decoration: const BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(24),
-              bottomRight: Radius.circular(24),
-            ),
-          ),
-          child: const Icon(
-            Icons.delete,
-            color: Colors.white,
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (_) => onDelete(),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        decoration: const BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(24),
+            bottomRight: Radius.circular(24),
           ),
         ),
-        child: Container(
-          height: 120,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 243, 243, 237),
-            borderRadius: BorderRadius.circular(24),
-            border: Border.all(color: const Color.fromARGB(255, 230, 230, 230)),
-          ),
-          child: Row(
-            children: [
-              // image
-              Container(
-                height: 120,
-                width: 150,
-                // TODO: if image, show, else default image
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      bottomLeft: Radius.circular(24),
-                    )),
-                child: const Icon(
-                  Icons.add_photo_alternate_outlined,
-                  size: 50,
-                  color: Colors.grey,
-                ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5.0),
+        height: 120,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 243, 243, 237),
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(color: const Color.fromARGB(255, 230, 230, 230)),
+        ),
+        child: Row(
+          children: [
+            // image
+            Container(
+              height: 120,
+              width: 150,
+              // TODO: if image, show, else default image
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    bottomLeft: Radius.circular(24),
+                  )),
+              child: const Icon(
+                Icons.add_photo_alternate_outlined,
+                size: 50,
+                color: Colors.grey,
               ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, left: 20),
-                    child: SizedBox(
-                      width: 120,
-                      child: Text(
-                        title,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                        ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20),
+                  child: SizedBox(
+                    width: 120,
+                    child: Text(
+                      title,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
                   ),
-                  Container(
-                    child: expiration != null && expiration != -1
-                        ? Padding(
-                            padding: const EdgeInsets.only(top: 5, left: 20),
-                            child: SizedBox(
-                              width: 120,
-                              child: Text(
-                                "$expiration days",
-                                textAlign: TextAlign.left,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Color.fromARGB(255, 66, 107, 31),
-                                ),
+                ),
+                Container(
+                  child: expiration != null && expiration != -1
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 5, left: 20),
+                          child: SizedBox(
+                            width: 120,
+                            child: Text(
+                              "$expiration days",
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 66, 107, 31),
                               ),
-                            ),
-                          )
-                        : Container(),
-                  ),
-                  Container(
-                    child: recipeDate != null
-                        ? Padding(
-                            padding: const EdgeInsets.only(top: 5, left: 20),
-                            child: SizedBox(
-                              width: 120,
-                              child: Text(
-                                "Saved ${DateFormat('M/d').format(recipeDate!)}",
-                                textAlign: TextAlign.left,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Color.fromARGB(255, 66, 107, 31),
-                                ),
-                              ),
-                            ),
-                          )
-                        : Container(),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Container(
-                child: qty != null && qty != -1
-                    ? Padding(
-                        padding: const EdgeInsets.only(top: 20, right: 20),
-                        child: Align(
-                          alignment: Alignment.topRight,
-                          child: Text(
-                            "x$qty",
-                            style: const TextStyle(
-                              fontSize: 20,
                             ),
                           ),
+                        )
+                      : Container(),
+                ),
+                Container(
+                  child: recipeDate != null
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 5, left: 20),
+                          child: SizedBox(
+                            width: 120,
+                            child: Text(
+                              "Saved ${DateFormat('M/d').format(recipeDate!)}",
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 66, 107, 31),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Container(
+              child: qty != null && qty != -1
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 20, right: 20),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          "x$qty",
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
-                      )
-                    : Container(),
-              ),
-              const SizedBox(width: 0),
-            ],
-          ),
+                      ),
+                    )
+                  : Container(),
+            ),
+            const SizedBox(width: 0),
+          ],
         ),
       ),
     );
